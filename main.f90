cce929808b359d2a541a1c35c6a6e67ff9ad6ea1
Varun Shah <varun.shah@postgrad.manchester.ac.uk>
2024-02-26 11:01:58 +0000

    Change default colour

--- main.f90
+++ main.f90
@@ -17,6 +17,9 @@ implicit none
     call read_hr ! r_list, r_ham_list, weights, num_r_pts, num_bands
     allocate(energies(nkp, num_bands), kham(num_bands, num_bands),             &
         colours(3, nkp, num_bands))
+    colours(1, :, :) = 0.5803922
+    colours(2, :, :) = 0.4039216
+    colours(3, :, :) = 0.7411765
 
     lwork=max(1, 2*num_bands-1)
     allocate(work(max(1, lwork)), rwork(max(1, 3*num_bands-2)))
@@ -25,7 +28,9 @@ implicit none
             num_r_pts)
         call zheev('V', 'L', num_bands, kham, num_bands, energies(ik, :), work,&
             lwork, rwork, info)
-        call get_colours(num_bands, norb, kham, colours(:, ik, :))
+        if (norb .neq. 1) then
+            call get_colours(num_bands, norb, kham, colours(:, ik, :))
+        end if
     end do
 
     call write_bands(nkp, num_bands, kdists, energies, colours)
